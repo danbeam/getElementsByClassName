524b0dfd0f8d6d9b998081e190c716c28fe76b14
anonymous <anonymous@funkyeah.getmyip.com>
2010-04-23 22:13:00 -0700

    Updated this to most recent version, beats the best one I found on the internet except for in FF2.  Also added newer style/formatting

--- getElementsByClassName.js
+++ getElementsByClassName.js
@@ -1,41 +1,39 @@
 /**
- * @author	Dan Beam, dan@danbeam.org
+ * @author	Dan Beam <dan@danbeam.org>
  * @internal	An easy way to find DOM Nodes with a specific class
- * @param	String:classToFind - the class we're looking for on DOM Nodes
+ * @param	String:className - the class we're looking for on DOM Nodes
  * @return	Array:results - any DOM Nodes that have the specified class
  */
 
-function getElementsByClassName( className )
-{
-	// if native support, use it
-	// (it "decimates" DOM parsing in the words of John Resig)
-	if( 'function' === typeof document.getElementsByClassName )
-	{
-		return document.getElementsByClassName( className );
-	}
+// if native support, use it
+// (it "decimates" DOM parsing in the words of John Resig)
+if ('function' !== typeof document.getElementsByClassName) {
 
-	for // otherwise do it ourselves
-	(
-		var	// this will be incremented to 0 at start of loop
-			i = -1,
+	// otherwise do it ourselves
+	document.getElementsByClassName = function (className) {
 
-			// results of the DOM query (elements with matching class)
-			results = [ ],
-
-			// regular expression to see if the class attribute contains
-			// the searched for class
-			finder = new RegExp( '(^|\\s)' + className + '(\\s|$)' ),
-
-			// grab all DOM elements and the set's length
-			a = document.all || document.getElementsByTagName('*'), len = a.length;
-
-		// this is done before we start and at every comparison (note the ++)
-		++i < len;
-
-		// this is done after the first comparison and every iteration afterward
-		finder.test( a[i].className ) && results.push( a[i] )
-	);
-
-	// do memory management and return the results of our query
-	delete a; return results;
+		for (
+			var	// this will be incremented to 0 at start of loop
+				i = -1,
+	
+				// results of the DOM query (elements with matching class)
+				results = [ ],
+	
+				// regular expression to see if the class attribute contains
+				// the searched for class
+				finder = new RegExp('(^|\\s)' + className + '(\\s|$)'),
+	
+				// grab all DOM elements and the set's length
+				a = document.all || document.getElementsByTagName('*'), len = a.length;
+	
+			// this is done before we start and at every comparison (note the ++)
+			++i < len;
+		
+			// this is done after the first comparison and every iteration afterward
+			finder.test(a[i].className) && results.push(a[i])
+		);
+	
+		// do memory management and return the results of our query
+		delete a; return results;
+	};
 }
