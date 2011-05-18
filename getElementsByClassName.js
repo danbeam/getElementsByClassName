c2006a3f94af9047600b64573d1c417bf6e1284c
Dan Beam <dan@danbeam.org>
2011-05-18 14:34:13 -0700

    small tweaks

--- getElementsByClassName.js
+++ getElementsByClassName.js
@@ -7,12 +7,11 @@
 * @return        {array} results - any DOM Nodes that have the specified class
 */
 
-// if native support, use it
-// (it "decimates" DOM parsing in the words of John Resig)
+// if native support, use it (it "decimates" DOM parsing in the words of John Resig)
 if ('function' !== typeof document.getElementsByClassName) {
 
-	// otherwise do it ourselves
-	document.getElementsByClassName = function (className, startFrom) {
+	// NOTE: HTMLElement will work in pretty much everything IE6+, but IE6 DOM Nodes inherit directly from Object (lame, right?)
+	('undefined' !== typeof HTMLElement ? HTMLElement : Object).prototype.getElementsByClassName = document.getElementsByClassName = function (testClass, startFrom) {
 
 		for (	var	// this will be incremented to 0 at start of loop
 				i = -1,
@@ -22,22 +21,25 @@ if ('function' !== typeof document.getElementsByClassName) {
 	
 				// regular expression to see if the class attribute contains
 				// the searched for class
-				finder = new RegExp('(^|\\s)' + className + '(\\s|$)'),
+				finder = new RegExp('(?:^|\\s)' + testClass+ '(?:\\s|$)'),
 	
 				// grab all DOM elements and the set's length
-				a = startFrom || document.all || document.getElementsByTagName('*'),
+				a = startFrom && startFrom.getElementsByTagName && startFrom.getElementsByTagName('*') || document.all || document.getElementsByTagName('*'),
 
 				// cache the length property
-				len = a.length;
+				l = a.length;
 	
 			// this is done before we start and at every comparison (note the ++)
-			++i < len;
+			++i < l;
 		
 			// this is done after the first comparison and every iteration afterward
 			finder.test(a[i].className) && results.push(a[i])
+
 		);
 	
 		// do memory management and return the results of our query
-		delete a; return results;
+		a = null; return results;
+
 	};
+
 }
